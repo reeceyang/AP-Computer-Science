74ccd00738171b5cb0923ba38a89514657bd7eb6
Reece Yang <reeceyang@live.com>
2017-03-01 21:11:04 -0600

    Added minimax tic-tac-toe algorithm. However, doesn't work for some reason. To-Do: Configure game option, find out what's wrong.

--- src/U7A3/Board.java
+++ src/U7A3/Board.java
@@ -119,53 +119,61 @@ public class Board
 */
 package U7A3;
 
-public class Board
-{
+public class Board {
     private Piece[][] pieces;
     private int boardLength;
-    public Board(int boardLength)
-    {
+    private int turn;
+
+    // Constructs a board filled with empty pieces and turn 1
+    public Board(int boardLength) {
         this.boardLength = boardLength;
         pieces = new Piece[boardLength][boardLength];
-        clear();
+        reset();
     }
-    public Board(String string)
-    {
-        boardLength = string.indexOf("\n");
+
+    // Constructs a board from a string
+    public Board(String string, int boardLength, int turn) {
+        this.turn = turn;
+        this.boardLength = boardLength;
         pieces = new Piece[boardLength][boardLength];
-        string.replace("\n", "");
-        for (int i = 0; i < boardLength; i++)
-        {
-            for (int j = 0; j < boardLength; j++)
-            {
-                pieces[i][j] = new Piece(string.charAt(i * boardLength + j));
+        string = string.replace("\n", "");
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = 0; j < boardLength; j++) {
+                pieces[i][j] = new Piece(string.charAt(i * boardLength + j) + "");
             }
         }
     }
-    public void place(int row, int col, Piece piece)
-    {
+
+    public void place(int row, int col, Piece piece) {
         pieces[row][col] = piece;
     }
-    public void clear()
-    {
-        for (int i = 0; i < boardLength; i++)
-        {
-            for (int j = 0; j < boardLength; j++)
-            {
+
+    public Board getState(int row, int col, Piece piece) {
+        Board possibleBoard = new Board(this.toString(), this.boardLength, this.turn);
+        possibleBoard.place(row, col, piece);
+        possibleBoard.nextTurn();
+        return possibleBoard;
+    }
+
+    // Resets the board and turn
+    public void reset() {
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = 0; j < boardLength; j++) {
                 pieces[i][j] = Piece.EMPTY;
             }
         }
+        turn = 1;
     }
-    public Piece getPiece(int row, int col)
-    {
+
+    public Piece getPiece(int row, int col) {
         return pieces[row][col];
     }
-    public Piece[][] getPieces()
-    {
+
+    public Piece[][] getPieces() {
         return pieces;
     }
-    public Piece checkForWin()
-    {
+
+    public Piece checkForWin() {
         int winner = 0;
         // Horizontal
         for (int i = 0; i < boardLength; i++) {
@@ -179,61 +187,128 @@ public class Board
             }
         }
         // Vertical
-        for (int i = 0; i < boardLength; i++) {
-            for (int j = 0; j < boardLength - 1; j++) {
-                if (!pieces[j][i].equals(pieces[j + 1][i])) {
-                    break;
-                }
-                if (j + 1 == boardLength - 1) {
-                    winner += pieces[0][i].getType();
+        if (winner == 0) {
+            for (int i = 0; i < boardLength; i++) {
+                for (int j = 0; j < boardLength - 1; j++) {
+                    if (!pieces[j][i].equals(pieces[j + 1][i])) {
+                        break;
+                    }
+                    if (j + 1 == boardLength - 1) {
+                        winner += pieces[0][i].getType();
+                    }
                 }
             }
         }
         // Diagonals
-        for (int i = 0; i < boardLength - 1; i++) {
-            if (!pieces[i][i].equals(pieces[i + 1][i + 1])) {
-                break;
-            }
-            if (i + 1 == boardLength - 1) {
-                winner += pieces[0][0].getType();
+        if (winner == 0) {
+            for (int i = 0; i < boardLength - 1; i++) {
+                if (!pieces[i][i].equals(pieces[i + 1][i + 1])) {
+                    break;
+                }
+                if (i + 1 == boardLength - 1) {
+                    winner += pieces[0][0].getType();
+                }
             }
         }
-        for (int i = 0; i < boardLength - 1; i++) {
-            if (!pieces[i][boardLength - 1 - i].equals(pieces[i + 1][boardLength - 2 - i])) {
-                break;
-            }
-            if (i + 1 == boardLength - 1) {
-                winner += pieces[0][boardLength - 1].getType();
+        if (winner == 0) {
+            for (int i = 0; i < boardLength - 1; i++) {
+                if (!pieces[i][boardLength - 1 - i].equals(pieces[i + 1][boardLength - 2 - i])) {
+                    break;
+                }
+                if (i + 1 == boardLength - 1) {
+                    winner += pieces[0][boardLength - 1].getType();
+                }
             }
         }
         return new Piece(winner);
     }
+
+    public int score() {
+        Piece player = getTurnPiece();
+        if (checkForWin().equals(player)) {
+            return 10;
+        } else if (checkForWin().equals(Piece.negate(player))) {
+            return -10;
+        } else {
+            return 0;
+        }
+    }
+
+    public Piece getTurnPiece() {
+        return turn % 2 == 1 ? Piece.X : Piece.O;
+    }
+
     // Checks if the board is filled with non-empty pieces
-    public boolean isFull()
-    {
-        for (int i = 0; i < boardLength; i++)
-        {
-            for (int j = 0; j < boardLength; j++)
-            {
-                if (pieces[i][j].equals(Piece.EMPTY))
-                {
-                    return false;
-                }
-            }
+    public boolean isFull() {
+        if (getNonEmptyPieces() != boardLength * boardLength) {
+            return false;
         }
         return true;
     }
-    public String toString()
-    {
+
+    public int getNonEmptyPieces() {
+        int nonEmptyPieces = 0;
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = 0; j < boardLength; j++) {
+                if (!pieces[i][j].equals(Piece.EMPTY)) {
+                    nonEmptyPieces++;
+                }
+            }
+        }
+        return nonEmptyPieces;
+    }
+
+    public int[][] getAvailableMoves() {
+        // availableMoves's structure
+        // Move Row Col
+        //  0    i   j
+        int[][] availableMoves = new int[boardLength * boardLength - getNonEmptyPieces()][2];
+        int index = 0;
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = 0; j < boardLength; j++) {
+                if (pieces[i][j].equals(Piece.EMPTY)) {
+                    availableMoves[index][0] = i;
+                    availableMoves[index][1] = j;
+                    index++;
+                }
+            }
+        }
+        return availableMoves;
+    }
+
+    public boolean isEndPosition() {
+        Piece winner = checkForWin();
+        // If there is a winner or the board is full
+        if (!winner.equals(Piece.EMPTY) || isFull()) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
         String string = "";
-        for (int i = 0; i < boardLength; i++)
-        {
-            for (int j = 0; j < boardLength; j++)
-            {
+        for (int i = 0; i < boardLength; i++) {
+            for (int j = 0; j < boardLength; j++) {
                 string += pieces[i][j];
             }
             string += "\n";
         }
         return string;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void nextTurn() {
+        turn++;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+
+    public int getBoardLength() {
+        return boardLength;
     }
 }
--- src/U7A3/TicTacToe.java
+++ src/U7A3/TicTacToe.java
@@ -4,56 +4,53 @@
 
 package U7A3;
 
-import javax.swing.JFrame;
 import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
-public class TicTacToe extends JFrame
-{
+public class TicTacToe extends JFrame {
     private JButton[] cells;
-    private JButton quitButton;
-    private JButton newGameButton;
     private JTextField jumbotron;
     private Board board;
     private JMenuBar menuBar;
     private JMenu fileMenu;
     private JMenu editMenu;
     private JMenu helpMenu;
+    private JMenuItem menuItem;
     private JPanel windowPanel;
     private JPanel cellPanel;
-    private int turn;
-    private static int BOARD_LENGTH = 3;
+    private boolean onePlayerMode = false;
+    private int BOARD_LENGTH = 3;
 
-    public TicTacToe()
-    {
+    public TicTacToe() {
         windowPanel = new JPanel();
         BorderLayout borderLayout = new BorderLayout();
         windowPanel.setLayout(borderLayout);
 
         setMenuBar();
-        setJumbotronPanel();
+        setJumbotron();
         setCellPanel();
         setFrame();
-        jumbotron.setText("X's Turn");
+        newGame();
     }
 
-    private void setFrame()
-    {
+    private void setFrame() {
         setContentPane(windowPanel);
         setJMenuBar(menuBar);
         setTitle("Tic-Tac-Toe");
@@ -63,27 +60,18 @@ public class TicTacToe extends JFrame
         setVisible(true);
     }
 
-    private void setCellPanel()
-    {
+    private void setCellPanel() {
         // Construct and set GridLayout layout manager
         cellPanel = new JPanel();
         GridLayout gL2 = new GridLayout(BOARD_LENGTH, BOARD_LENGTH);
         cellPanel.setLayout(gL2);
 
-        // Set turn to 1
-        turn = 1;
-
         cells = new JButton[BOARD_LENGTH * BOARD_LENGTH];
         board = new Board(BOARD_LENGTH);
-        class CellActionListener implements ActionListener
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+        class CellActionListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
                 JButton clickedButton = (JButton) e.getSource();
-                if (clickedButton.getText().equals("X") || clickedButton.getText().equals("O"))
-                {/* The cell is filled, do nothing */}
-                else
-                {
+                if (!clickedButton.getText().equals("X") && !clickedButton.getText().equals("O")) {
                     // The cell is empty
                     // Find which button was clicked
                     int clickedButtonHashCode = clickedButton.hashCode();
@@ -95,51 +83,24 @@ public class TicTacToe extends JFrame
                         }
                     }
                     // Find out who's turn it is
-                    Piece mover = turn % 2 == 1 ? Piece.X : Piece.O;
+                    Piece mover = board.getTurnPiece();
                     // Set the clicked button's text to that piece
                     cells[index].setText(mover.toString());
                     // Place the piece in the corresponding cell on the board
                     board.place(index / BOARD_LENGTH, index % BOARD_LENGTH, mover);
-                    // Check for a winner
-                    Piece winner = board.checkForWin();
-                    // If there is no winner and the board isn't full
-                    if (winner.equals(Piece.EMPTY) && !board.isFull())
-                    {
+                    System.out.println("Button clicked");
+                    if (board.isEndPosition()) {
+                        showGameOverDialog();
+                    } else {
                         // Next turn
-                        jumbotron.setText((mover.toString().equals("X") ? "O" : "X")
-                                + jumbotron.getText().substring(1));
-                        turn++;
+                        board.nextTurn();
+                        updateJumbotron();
                     }
-                    else
-                    {
-                        // Find out if it's a tie or if X or O won
-                        String message = winner.equals(Piece.EMPTY) ?
-                                        "The game has tied."
-                                        : winner + " has won the game.";
-                        // Show option dialog with quit and new game choices
-                        Object[] choices = {"Quit", "New Game"};
-                        int chosen = JOptionPane.showOptionDialog(null,
-                                message,
-                                "Tic-Tac-Toe",
-                                JOptionPane.YES_NO_OPTION,
-                                JOptionPane.QUESTION_MESSAGE,
-                                null,
-                                choices,
-                                choices[1]);
-                        if (chosen == 0)
-                        {
-                            quit();
-                        }
-                        else
-                        {
-                            newGame();
-                        }
-                    }
+                    computerMove();
                 }
             }
         }
-        for (int i = 0; i < BOARD_LENGTH * BOARD_LENGTH; i++)
-        {
+        for (int i = 0; i < BOARD_LENGTH * BOARD_LENGTH; i++) {
             cells[i] = new JButton("");
             ActionListener cellListener = new CellActionListener();
             cells[i].addActionListener(cellListener);
@@ -148,58 +109,10 @@ public class TicTacToe extends JFrame
         windowPanel.add("Center", cellPanel);
     }
 
-    private void setMenuBar()
-    {
+    private void setMenuBar() {
         menuBar = new JMenuBar();
-        // A recyclable menu item
-        JMenuItem menuItem;
 
-        fileMenu = new JMenu("File");
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        menuItem = new JMenuItem("New Game", KeyEvent.VK_N);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription(
-                "Start a new game");
-        class NewGameActionListener implements ActionListener
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                newGame();
-            }
-        }
-        menuItem.addActionListener(new NewGameActionListener());
-        fileMenu.add(menuItem);
-        menuItem = new JMenuItem("Export Game", KeyEvent.VK_E);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_E, ActionEvent.CTRL_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription(
-                "Export the current game");
-        class ExportGameActionListener implements ActionListener
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                export();
-            }
-        }
-        menuItem.addActionListener(new ExportGameActionListener());
-        fileMenu.add(menuItem);
-        fileMenu.addSeparator();
-        menuItem = new JMenuItem("Quit", KeyEvent.VK_Q);
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription(
-                "Quit the game");
-        class QuitActionListener implements ActionListener
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                quit();
-            }
-        }
-        menuItem.addActionListener(new QuitActionListener());
-        fileMenu.add(menuItem);
-        menuBar.add(fileMenu);
+        setFileMenu();
 
         editMenu = new JMenu("Edit");
         editMenu.setMnemonic(KeyEvent.VK_E);
@@ -212,10 +125,8 @@ public class TicTacToe extends JFrame
                 KeyEvent.VK_A, ActionEvent.CTRL_MASK + ActionEvent.SHIFT_MASK));
         menuItem.getAccessibleContext().setAccessibleDescription(
                 "About Tic-Tac-Toe");
-        class AboutActionListener implements ActionListener
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+        class AboutActionListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null,
                         "Tic-Tac-Toe v1.0 \u00a9 Reece Yang 2017");
             }
@@ -225,52 +136,162 @@ public class TicTacToe extends JFrame
         menuBar.add(helpMenu);
     }
 
-    private void setJumbotronPanel()
-    {
+    private void setFileMenu() {
+        fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        menuItem = new JMenuItem("New Game", KeyEvent.VK_N);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "Start a new game");
+        class NewGameActionListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                newGame();
+            }
+        }
+        menuItem.addActionListener(new NewGameActionListener());
+        fileMenu.add(menuItem);
+
+        menuItem = new JMenuItem("Export Game", KeyEvent.VK_E);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "Export the current game");
+        class ExportGameActionListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                export();
+            }
+        }
+        menuItem.addActionListener(new ExportGameActionListener());
+        fileMenu.add(menuItem);
+
+        menuItem = new JMenuItem("Import Game", KeyEvent.VK_I);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_I, ActionEvent.CTRL_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "Import a saved game");
+        class ImportGameActionListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                open();
+            }
+        }
+        menuItem.addActionListener(new ImportGameActionListener());
+        fileMenu.add(menuItem);
+
+        fileMenu.addSeparator();
+
+        menuItem = new JMenuItem("Quit", KeyEvent.VK_Q);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "Quit the game");
+        class QuitActionListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                Utilities.quit();
+            }
+        }
+        menuItem.addActionListener(new QuitActionListener());
+        fileMenu.add(menuItem);
+
+        menuBar.add(fileMenu);
+    }
+
+    private void setJumbotron() {
         jumbotron = new JTextField(30);
         jumbotron.setHorizontalAlignment(JTextField.CENTER);
         jumbotron.setEditable(false);
         windowPanel.add("North", jumbotron);
     }
 
-    private void refreshBoard()
-    {
-        for (int i = 0; i < BOARD_LENGTH * BOARD_LENGTH; i++)
-        {
-            cells[i].setText(board.getPiece(i / BOARD_LENGTH, i % BOARD_LENGTH).toString());
+    private void showGameOverDialog() {
+        Piece winner = board.checkForWin();
+        // Find out if it's a tie or if X or O won
+        String message = winner.equals(Piece.EMPTY) ?
+                "The game has tied."
+                : winner + " has won the game.";
+        // Show option dialog with quit and new game choices
+        Object[] choices = {"Quit", "New Game"};
+        int chosen = JOptionPane.showOptionDialog(null,
+                message,
+                "Tic-Tac-Toe",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                choices,
+                choices[1]);
+        if (chosen == 0) {
+            Utilities.quit();
+        } else {
+            newGame();
         }
     }
 
-    private void newGame()
-    {
-        board.clear();
+    private void computerMove() {
+        if (onePlayerMode) {
+            PerfectPlayer reeceYang = new PerfectPlayer(board.getTurnPiece(), board);
+            reeceYang.minimax(board);
+            board.place(reeceYang.getChoice()[0], reeceYang.getChoice()[1], reeceYang.getMe());
+            refreshBoard();
+            if (board.isEndPosition()) {
+                showGameOverDialog();
+            } else {
+                // Next turn
+                board.nextTurn();
+                updateJumbotron();
+            }
+        }
+    }
+
+    private void refreshBoard() {
+        for (int i = 0; i < BOARD_LENGTH * BOARD_LENGTH; i++) {
+            cells[i].setText(board.getPiece(i / BOARD_LENGTH, i % BOARD_LENGTH).toString());
+        }
+        System.out.println("Board refreshed");
+        updateJumbotron();
+    }
+
+    private void updateJumbotron() {
+        jumbotron.setText(board.getTurnPiece() + "'s turn");
+    }
+
+    private void newGame() {
+        board.reset();
         refreshBoard();
-        turn = 1;
         jumbotron.setText("X's Turn");
     }
 
-    private void quit()
-    {
-        System.exit(0);
-    }
-
-    private void export()
-    {
-        try
-        {
-            FileWriter writer = new FileWriter("src/U7A3/board.brd");
-            PrintWriter out = new PrintWriter(writer);
-            out.println(board.toString());
-            out.close();
-        }
-        catch (IOException e)
-        {
+    private void open() {
+        try {
+            Scanner in = new Scanner(new File("src/U7A3/board.brd"));
+            int boardLength = in.nextInt();
+            int turn = in.nextInt();
+            String boardString = "";
+            for (int i = 0; i <= boardLength; i++) {
+                boardString += in.nextLine();
+            }
+            board = new Board(boardString, boardLength, turn);
+            refreshBoard();
+            in.close();
+        } catch (IOException e) {
             throw new RuntimeException(e.toString());
         }
     }
 
-    public static void main(String[] args)
-    {
+    private void export() {
+        try {
+            FileWriter writer = new FileWriter("src/U7A3/board.brd");
+            PrintWriter out = new PrintWriter(writer);
+            out.println(board.getBoardLength());
+            out.println(board.getTurn());
+            out.println(board.toString());
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e.toString());
+        }
+    }
+
+    public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
     }
 }
