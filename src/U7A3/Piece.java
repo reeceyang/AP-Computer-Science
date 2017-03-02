f78cfc331176d3460f28bfe154651f38a5f6833c
Yang.Reece.21 <ReYan21@icstudents.org>
2017-03-02 10:27:21 -0600

    Start to redo U7A3

--- src/U7A3/Piece.java
+++ src/U7A3/Piece.java
@@ -1,89 +1,35 @@
-/*//Ben Schroeder
+// Colleague #1
 //
-//Piece Class
+// This is the code for the Piece class.
+// In the Tic-Tac-Toe game, a Piece will be either
+// an X or an O.
 
 package U7A3;
 
 public class Piece
 {
-	private String pieceType;
+	private String id;
 
-	public Piece(String player)
+	public Piece()
 	{
-		if(player.equals("p1"))
-			pieceType = "X";
-		else if(player.equals("p2"))
-			pieceType = "O";
+		id = " ";
 	}
 
-	public String getPiece()
+	public Piece(String x)
 	{
-		return pieceType;
+		id = x;
 	}
-}
-*/
-package U7A3;
 
-public class Piece {
-    private int type;
-    public static final Piece EMPTY = new Piece(0);
-    public static final Piece X = new Piece(1);
-    public static final Piece O = new Piece(2);
+	public String getID()
+	{
+		return id;
+	}
 
-    public Piece(int type) {
-        this.type = type;
-    }
-
-    public Piece(String type) {
-        this.type = stringToType(type);
-    }
-
-    public int getType() {
-        return type;
-    }
-
-    public String toString() {
-        return typeToString(type);
-    }
-
-    public boolean equals(Object object) {
-        // Pieces are equal if they have the same type
-        Piece piece = (Piece) object;
-        return piece.getType() == type;
-    }
-
-    public static Piece negate(Piece piece) {
-        if (piece.equals(Piece.X)) {
-            return Piece.O;
-        } else if (piece.equals(O)){
-            return Piece.X;
-        }
-        return Piece.EMPTY;
-    }
-
-    public static int stringToType(String string) {
-        switch (string) {
-            case " ":
-                return 0;
-            case "X":
-                return 1;
-            case "O":
-                return 2;
-            default:
-                return 0;
-        }
-    }
-
-    public static String typeToString(int type) {
-        switch (type) {
-            case 0:
-                return " ";
-            case 1:
-                return "X";
-            case 2:
-                return "O";
-            default:
-                return "Invalid Type";
-        }
-    }
+	public boolean equals(Piece p)
+	{
+		if (this.getID().equals(p.getID()))
+			return true;
+		else
+			return false;
+	}
 }
