bc82f3b3b89494acebad42f3e13de1d041618823
Yang.Reece.21 <ReYan21@icstudents.org>
2017-05-05 10:06:20 -0500

    Finish final project

---
+++ src/Final Project #1/Polynomial.java
@@ -0,0 +1,159 @@
+/**
+ * This class models a polynomial. It has an ArrayList of Term objects
+ * and methods to add, subtract, and multiply other polynomials.
+ *
+ * @author Reece Yang
+ */
+
+import java.util.ArrayList;
+
+public class Polynomial
+{
+    private ArrayList<Term> terms;
+
+    /**
+     * Class constructor that specifies the terms of the polynomial.
+     *
+     * @param someTerms the terms of the polynomial.
+     */
+    public Polynomial(ArrayList<Term> someTerms)
+    {
+        terms = someTerms;
+        setStandardForm();
+    }
+
+    /**
+     * Class constructor that constructs a polynomial with the specified term.
+     *
+     * @param term the single term of the polynomial.
+     */
+    public Polynomial(Term term)
+    {
+        ArrayList<Term> temp = new ArrayList<>();
+        temp.add(term);
+        terms = temp;
+        setStandardForm();
+    }
+
+    public ArrayList<Term> getTerms()
+    {
+        return terms;
+    }
+
+    /**
+     * Multiplies the current polynomial by the specified polynomial.
+     *
+     * @param polynomial the polynomial to multiply this by.
+     * @return the product of the current polynomial and the specified polynomial
+     */
+    public Polynomial multiply(Polynomial polynomial)
+    {
+        ArrayList<Term> otherTerms = polynomial.getTerms();
+        ArrayList<Term> newTerms = new ArrayList<>();
+        for (Term otherTerm : otherTerms)
+        {
+            for (Term term : terms)
+            {
+                newTerms.add(otherTerm.multiply(term));
+            }
+        }
+        return new Polynomial(newTerms);
+    }
+
+    /**
+     * Multiplies the current polynomial by the specified term.
+     *
+     * @param term the term to multiply the current polynomial by.
+     * @return the product of the current polynomial and the specified term.
+     */
+    public Polynomial multiply(Term term)
+    {
+        ArrayList<Term> temp = new ArrayList<>();
+        temp.add(term);
+        Polynomial p = new Polynomial(temp);
+        return multiply(p);
+    }
+
+    /**
+     * Adds the current polynomial and the specified polynomial.
+     *
+     * @param polynomial the polynomial to add to the current polynomial.
+     * @return the sum of the specified polynomial and the current polynomial.
+     */
+    public Polynomial add(Polynomial polynomial)
+    {
+        ArrayList<Term> newTerms = new ArrayList<>();
+        for (Term term : terms)
+        {
+            newTerms.add(term);
+        }
+        for (Term term : polynomial.getTerms())
+        {
+            newTerms.add(term);
+        }
+        return new Polynomial(newTerms);
+    }
+
+    /**
+     * Subtracts the specified polynomial from the current polynomial.
+     *
+     * @param polynomial the polynomial to subtract from the current polynomial.
+     * @return the difference of the current polynomial and the specified polynomial.
+     */
+    public Polynomial subtract(Polynomial polynomial)
+    {
+        return this.add(polynomial.multiply(new Term(-1, 0)));
+    }
+
+    /**
+     * Puts the polynomial in standard form. The terms are arranged so that the
+     * terms with the greatest degrees are first.
+     */
+    private void setStandardForm()
+    {
+        addLikeTerms();
+        // Sort terms by degree
+        terms.sort((o1, o2) -> o1.getDegree() < o2.getDegree() ? 1 : -1);
+    }
+
+    /**
+     * Adds the like terms in the polynomial. Like terms have the same degree.
+     */
+    private void addLikeTerms()
+    {
+        int i = 0;
+        while (i < terms.size())
+        {
+            for (int j = i + 1; j < terms.size(); j++)
+            {
+                if (terms.get(i).getDegree() == terms.get(j).getDegree())
+                {
+                    terms.add(new Term(terms.get(i).getCoefficient()
+                            + terms.get(j).getCoefficient(), terms.get(i).getDegree()));
+                    terms.remove(j);
+                    terms.remove(i);
+                    i--;
+                    break;
+                }
+            }
+            i++;
+        }
+    }
+
+    /**
+     * Converts the current polynomial to a string.
+     *
+     * @return the current polynomial in string form.
+     */
+    public String toString()
+    {
+        String string = "";
+        for (Term term : terms)
+        {
+            string += term + " + ";
+        }
+        string = string.replaceAll("\\+ -", "- ");
+        string = string.substring(0, string.length() - 2);
+        return string;
+    }
+}
---
+++ src/Final Project #1/Term.java
@@ -0,0 +1,118 @@
+/**
+ * This class models a term of the form ax^n in a polynomial,
+ * where a and n are integers and n is non-negative.
+ *
+ * @author Reece Yang
+ */
+
+public class Term
+{
+    private int coefficient;
+    private int degree;
+
+    /**
+     * Class constructor that specifies the coefficient and degree of the term.
+     * Precondition: aDegree is non-negative.
+     *
+     * @param aCoefficient the coefficient of the term.
+     * @param aDegree      the degree of the term.
+     */
+    public Term(int aCoefficient, int aDegree)
+    {
+        coefficient = aCoefficient;
+        degree = aDegree;
+    }
+
+    public int getCoefficient()
+    {
+        return coefficient;
+    }
+
+    public int getDegree()
+    {
+        return degree;
+    }
+
+    /**
+     * Returns the product of the current term and the specified term.
+     *
+     * @param term the term to multiply the current term by.
+     * @return the product of the specified term and the current term.
+     */
+    public Term multiply(Term term)
+    {
+        int newCoefficient = term.getCoefficient() * coefficient;
+        int newDegree = term.getDegree() + degree;
+        return new Term(newCoefficient, newDegree);
+    }
+
+    /**
+     * Converts the current term to a string [coefficient]x^[degree].
+     * If the degree is 0 or the coefficient is 0 the x is omitted,
+     * if the degree is 1 the exponent is omitted,
+     * and if the coefficient is 1 or -1 the coefficient is omitted.
+     *
+     * @return the term as a string.
+     */
+    public String toString()
+    {
+        String string = "";
+        if (degree == 0)
+        {
+            string += coefficient;
+        }
+        else
+        {
+            if (coefficient != 1)
+            {
+                if (coefficient == -1)
+                {
+                    string += "-";
+                }
+                else
+                {
+                    string += coefficient;
+                }
+            }
+            if (coefficient != 0 && degree != 0)
+            {
+                string += "x";
+            }
+            if (degree > 1 && coefficient != 0)
+            {
+                string += "^" + degree;
+            }
+        }
+        return string;
+    }
+
+    /**
+     * Checks if the specified object is equal to the current term.
+     * Two terms are equal if their coefficients and degrees are equal,
+     * or if their coefficient is 0.
+     *
+     * @param object the object to compare to the current term.
+     * @return true if the object is equal to the current term, false otherwise.
+     */
+    public boolean equals(Object object)
+    {
+        if (object instanceof Term)
+        {
+            Term otherTerm = (Term) object;
+            if (otherTerm.getCoefficient() == coefficient
+                    && otherTerm.getDegree() == degree)
+            {
+                return true;
+            }
+            else if (otherTerm.getCoefficient() == 0 && coefficient == 0) {
+                return true;
+
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
